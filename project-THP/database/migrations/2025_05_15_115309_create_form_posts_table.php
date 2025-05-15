02120c4d011f42c98c769e155de8997fcc5646f5
waleed khalid <151838143+waleedarman@users.noreply.github.com>
2025-05-15 15:45:05 +0300

    create model and migration

---
+++ project-THP/database/migrations/2025_05_15_115309_create_form_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_posts', function (Blueprint $table) {
+            $table->id('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('minimum_budget', 10, 2);
+            $table->decimal('maximum_budget', 10, 2);
+            $table->date('deadline');
+            $table->string('category');
+            $table->string('location');
+            $table->string('attachments')->nullable();
+
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_posts');
+    }
+};
