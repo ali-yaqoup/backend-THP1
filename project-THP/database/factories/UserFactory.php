886282aeeebd839c9ba77993863c6eceb226c85d
Kareem Dwekat <149584641+kareem-Dwekat@users.noreply.github.com>
2025-05-17 17:52:02 +0300

    backend3

--- project-THP/database/factories/UserFactory.php
+++ project-THP/database/factories/UserFactory.php
@@ -1,42 +1,31 @@
 <?php
 
+
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'full_name' => $this->faker->name,
+            'username' => $this->faker->userName,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password123'),
+            'user_type' => $this->faker->randomElement(['job_owner', 'artisan']),
+        ];
+    }
 
     /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
+     * إرجاع حالة البريد الإلكتروني كغير مُحقق.
      */
- public function definition()
-     {
-         return [
-             // تعديل العمود من name إلى full_name
-             'full_name' => $this->faker->name,
-             'username' => $this->faker->userName,
-             'email' => $this->faker->unique()->safeEmail,
-             'password' => Hash::make('password123'),
-             'user_type' => $this->faker->randomElement(['job_owner', 'artisan']),
-         ];
-     }
- }
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function unverified(): self
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
