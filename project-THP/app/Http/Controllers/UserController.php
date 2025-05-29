38750b5eded5bc842d5d9d3f02d9ceb4b80d55c1
Ali Yaqoub <149534456+ali-yaqoup@users.noreply.github.com>
2025-05-29 11:35:51 +0300

    Update UserController.php

--- project-THP/app/Http/Controllers/UserController.php
+++ project-THP/app/Http/Controllers/UserController.php
@@ -1,33 +1,57 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
 
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid email or password.'
+            ], 401);
+        }
+
+        if ($user->status !== 'approved') {
+            return response()->json([
+                'message' => 'Your account is not approved or has been rejected.',
+                'status' => $user->status
+            ], 403);
+        }
+
+
+        return response()->json([
+            'message' => 'Login successful.',
+            'user' => $user
+        ], 200);
+    }
     public function register(RegisterRequest $request)
     {
         try {
-            // إنشاء المستخدم
+
+            $validated = $request->validated();
+
+
             $user = User::create([
-                'full_name' => $request->full_name,
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'user_type' => $request->user_type,
+                'full_name' => $validated['full_name'],
+                'username' => $validated['username'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+                'user_type' => $validated['user_type'],
+                'status' => 'pending',
             ]);
 
-            // إرسال رابط التحقق عبر البريد الإلكتروني
-            $user->sendEmailVerificationNotification();
 
             return response()->json([
-                'message' => 'User registered successfully! Please check your email to verify your account.',
+                'message' => 'User registered successfully!',
                 'user' => $user
             ], 201);
         } catch (\Exception $e) {
